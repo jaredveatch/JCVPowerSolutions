c97471799a9ed04e651a76cb282686cba4de2774
Jared Veatch <jared.veatch1@gmail.com>
2026-05-21 22:07:44 -0500

    Fix create job routes

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
     path("", views.home, name="home"),
 
     path("dashboard/", views.dashboard, name="dashboard"),
@@ -12,12 +13,36 @@ urlpatterns = [
 
     path("customers/", views.customers, name="customers"),
     path("customers/create/", views.create_customer, name="create_customer"),
-    path("customers/<int:customer_id>/", views.customer_detail, name="customer_detail"),
-    path("customers/<int:customer_id>/delete/", views.delete_customer, name="delete_customer"),
 
-    path("customers/<int:customer_id>/create-job/", views.create_job, name="create_job"),
+    path(
+        "customers/<int:customer_id>/",
+        views.customer_detail,
+        name="customer_detail"
+    ),
+
+    path(
+        "customers/<int:customer_id>/delete/",
+        views.delete_customer,
+        name="delete_customer"
+    ),
+
+    path(
+        "customers/<int:customer_id>/create-job/",
+        views.create_job,
+        name="create_job"
+    ),
 
     path("jobs/", views.jobs, name="jobs"),
-    path("jobs/<int:job_id>/", views.job_detail, name="job_detail"),
-    path("jobs/<int:job_id>/create-estimate/", views.create_estimate_from_job, name="create_estimate_from_job"),
+
+    path(
+        "jobs/<int:job_id>/",
+        views.job_detail,
+        name="job_detail"
+    ),
+
+    path(
+        "jobs/<int:job_id>/create-estimate/",
+        views.create_estimate_from_job,
+        name="create_estimate_from_job"
+    ),
 ]
