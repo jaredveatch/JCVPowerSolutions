14b07b2b04d78f647f48e08f1838781d1de425b7
Jared Veatch <jared.veatch1@gmail.com>
2026-05-21 20:54:37 -0500

    Add customers route

--- core/urls.py
+++ core/urls.py
@@ -2,34 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.home, name="home"),
+    path("dashboard/", views.dashboard, name="dashboard"),
 
-    path(
-        "",
-        views.home,
-        name="home"
-    ),
+    path("leads/", views.leads, name="leads"),
+    path("leads/<int:lead_id>/", views.lead_detail, name="lead_detail"),
+    path("leads/<int:lead_id>/convert/", views.convert_lead, name="convert_lead"),
 
-    path(
-        "dashboard/",
-        views.dashboard,
-        name="dashboard"
-    ),
-
-    path(
-        "leads/",
-        views.leads,
-        name="leads"
-    ),
-
-    path(
-        "leads/<int:lead_id>/",
-        views.lead_detail,
-        name="lead_detail"
-    ),
-
-    path(
-        "leads/<int:lead_id>/convert/",
-        views.convert_lead,
-        name="convert_lead"
-    ),
+    path("customers/", views.customers, name="customers"),
 ]
